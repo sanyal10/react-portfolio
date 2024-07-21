0d526d6bc95aa17fa4940b9e063acae68a62b84b
sanyal10 <sanyaldebajyoti2003@gmail.com>
2024-07-21 21:41:11 +0530

    update

--- src/content_option.js
+++ src/content_option.js
@@ -1,144 +1,149 @@
-const logotext = "JOHN";
+import img1  from "./src_files/img_1.png";
+import img2  from "./src_files/img2.jpg";
+import img3  from "./src_files/img3.jpg";
+import img4  from "./src_files/img4.jpg";
+import img5  from "./src_files/img5.jpg";
+import img6  from "./src_files/img6.png";
+import img7  from "./src_files/img7.png";
+import img8  from "./src_files/img8.png";
+import img9  from "./src_files/img_9.jpg";
+
+
+const logotext = "sanyal_10";
+
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Debajyoti Sanyal",
+    description: "Front-end Developer",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Debajyoti Sanyal",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I code cool websites",
+        second: "I develop Front-end of a web applications",
+        third: "Love to build Computer Vision applications",
+        fourth:"Love to build Computer Vision applications"
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Great to see you here. Check out my projects in the portfolio section.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "Introduction",
+    aboutme: "Hey !! this is Debajyoti, a recent engineering graduate. I am a Software Developer with a diverse range of interests in technology, this includes web development, machine learning, and IoT technologies. I have  two years of experience in frontend development. I have expertise in Python and JavaScript, and I’ve contributed to several open-source projects. My portfolio showcases my ability to build scalable web applications, and I pride myself on writing clean, efficient code.My eagerness to learn and my versatile skillset demonstrate my ability to adapt to new challenges and my desire to expand my knowledge and expertise in various areas of technology.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Front-end Developer Intern",
+        where: "Foodlabs",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Machine learning Intern",
+        where: "Cognizant",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Open-source-Contributer",
+        where: "Face-x (GSSOC)",
+        date: "2022",
     },
+
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
+const skills = [
+    {
+        name: "HTML",
+        value: 80,
     },
     {
-        name: "Djano",
-        value: 85,
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 90,
     },
     {
         name: "Javascript",
         value: 80,
     },
     {
-        name: "React",
+        name: "React.Js",
         value: 60,
     },
     {
-        name: "Jquery",
+        name: "Node.js",
+        value: 50,
+    },
+    {
+        name: "MySql",
         value: 85,
+    },
+    {
+        name: "AWS Cloud",
+        value: 60,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Frontend-Development",
+        description: "I build exceptional and accesible digital web experiences with stunning visual appeal and responsive.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img1,
+        description: "Landing page for Trekking Agency",
+        link: "https://github.com/sanyal10/Loss-Angeles-Mountain",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img2,
+        description: "Space Invader game using HTML, CSS and Javascript",
+        link: "https://github.com/sanyal10/Space-Invader-Game",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img3,
+        description: "Tic Tac Toe game using HTML, CSS and Javascript.",
+        link: "https://github.com/sanyal10/TIC-TAC-TOE-Game-",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img4,
+        description: "Calculator App using React.js ",
+        link: "https://github.com/sanyal10/Simple-Calculator",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img5,
+        description: "Air Bnb landing page using React.js",
+        link: "https://github.com/sanyal10/AirBnb-clone-",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img:img6,
+        description: "Foodlabs: A food ordering landing page",
+        link: "https://github.com/sanyal10/Foodlabs",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img7,
+        description: "Attendance registration application",
+        link: "https://github.com/sanyal10/Attendance-registration-",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: img8,
+        description: "Background Generator ",
+        link: "https://github.com/sanyal10/Bg-generator",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+        img: img9,
+        description: "Digital-Inspection-System",
+        link: "https://github.com/sanyal10/Digital-Inspection-System-using-Yolo-v8",
+    }
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "sanyaldebajyoti2003@gmail.com",
+    YOUR_FONE: "",
+    description: "Fill this form to send message directly to me.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,10 +152,9 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/sanyal10",
+    linkedin: "https://www.linkedin.com/in/debajyoti-sanyal-21023021a/",
+    twitter: "https://twitter.com/_pegasus_03",
 };
 export {
     meta,
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -37,7 +37,7 @@ export const About = () => {
         </Row>
         <Row className=" sec_sp">
           <Col lg="5">
-            <h3 className="color_sec py-4">Work Timline</h3>
+            <h3 className="color_sec py-4">Experience</h3>
           </Col>
           <Col lg="7">
             <table className="table caption-top">
@@ -54,7 +54,7 @@ export const About = () => {
               </tbody>
             </table>
           </Col>
-        </Row>
+        </Row> 
         <Row className="sec_sp">
           <Col lg="5">
             <h3 className="color_sec py-4">Skills</h3>
